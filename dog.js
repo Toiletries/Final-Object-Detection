ed46dbc753c72f73e1c617384bc6e03af13fc268
Toiletries <92912011+Toiletries@users.noreply.github.com>
2023-04-26 18:12:25 +1000

    Add files via upload

---
+++ dog.js
@@ -0,0 +1,43 @@
+img = "";
+objects=[];
+status="";
+function preload(){
+  img = loadImage('dog_cat.jpg');
+}
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML="status : Detecting objects";
+}
+
+function modelLoaded(){
+  console.log("Model Loaded!")
+  status= true
+  objectDetector.detect(img,gotResult) 
+
+}
+function gotResult(error, results){
+  if (error) {
+    console.log(error)
+  } else {
+    objects=results
+    console.log(objects)
+  }
+}
+function draw() {
+  image(img,0,0,640,420);
+  if (status !="") {
+    fill("#FF0000");
+    noFill();
+    stroke("#FF0000");
+    for (let i = 0; i < objects.length; i++) {
+      percent=floor(objects[i].confidence*100)
+      text(objects[i].label +" "+percent+"%",objects[i].x,objects[i].y)
+      rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+      document.getElementById("status").innerHTML="status : Detected objects";
+
+    }
+ }
+}
